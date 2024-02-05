331d0db1c72cbcf356d0d8af6a915f777b12f572
M. Doni Saputra <97510153+mdonisaputra@users.noreply.github.com>
2024-02-05 14:03:34 +0700

    Update lagi cards.tsx

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,7 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
-import { roboto, lusitana } from '@/app/ui/fonts';
+import { roboto } from '@/app/ui/fonts';
 import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
